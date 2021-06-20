a6624bcf9b0a7810b9d84337dae789c9ed0118b3
SINA-1998 <sinafallahnezhad76@gmail.com>
2021-06-20 11:17:34 +0430

    Remove api view. Create templates directory in blog app and use render shortcut for show home.html file in home view.

--- blog/views.py
+++ blog/views.py
@@ -4,35 +4,4 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World! Hello Django!")
-
-
-def api(request):
-    data = {
-        "1": {
-            "title": "first article",
-            "id": 1,
-            "slug": "first-article"
-        },
-        "2": {
-            "title": "second article",
-            "id": 2,
-            "slug": "second-article"
-        },
-        "3": {
-            "title": "third article",
-            "id": 3,
-            "slug": "third-article"
-        },
-        "4": {
-            "title": "fourth article",
-            "id": 4,
-            "slug": "fourth-article"
-        },
-        "5": {
-            "title": "fiveth article",
-            "id": 5,
-            "slug": "fiveth-article"
-        }
-    }
-    return JsonResponse(data)
+    return render(request, 'blog/home.html')
