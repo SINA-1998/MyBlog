11bfe1c3f7c1352a1fc30e3aa51d9b41cb58b901
SINA-1998 <sinafallahnezhad76@gmail.com>
2021-06-21 18:50:13 +0430

    Farsi admin panel.

--- blog/models.py
+++ blog/models.py
@@ -5,17 +5,21 @@ from django.utils import timezone
 # Create your models here.
 class Article(models.Model):
     STATUS_CHOICES = (
-        ("d", "Draft"),
-        ("p", "Published")
+        ("d", "پیش‌نویس"),
+        ("p", "منتشر شده")
     )
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to="images")
-    publish = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=250, verbose_name="عنوان")
+    slug = models.SlugField(max_length=250, unique=True, verbose_name="اسلاگ مقاله")
+    description = models.TextField(verbose_name="توضیحات")
+    thumbnail = models.ImageField(upload_to="images", verbose_name="تصویر مقاله")
+    publish = models.DateTimeField(default=timezone.now, verbose_name="زمان انتشار")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name="وضغیت")
 
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
+        
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
@@ -13,6 +12,6 @@ def home(request):
 
 def detail(request, slug):
     context = {
-        "article": Article.objects.get(slug=slug)
+        "article": get_object_or_404(Article, slug=slug, status="p")
     }
     return render(request, 'blog/detail.html', context)
