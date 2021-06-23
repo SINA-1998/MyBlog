0f6cc49e52c3375486fd21fb1686d31b181d6afb
SINA-1998 <sinafallahnezhad76@gmail.com>
2021-06-23 16:18:25 +0430

    Convert detail view to class view (DetailView).

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ArticleList, detail, category
+from .views import ArticleList, ArticleDetailView, category
 
 app_name = 'blog'
 urlpatterns = [
     path('', ArticleList.as_view(), name='home'),
     path('page/<int:page>', ArticleList.as_view(), name='home'),
-    path('article/<slug:slug>', detail, name='detail'),
+    path('article/<slug:slug>', ArticleDetailView.as_view(), name='detail'),
     path('category/<slug:slug>', category, name='category'),
     path('category/<slug:slug>/page/<int:page>', category, name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,31 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from .models import Article, Category
 from django.core.paginator import Paginator
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 
 # Create your views here.
-# def home(request, page=1):
-#     articles_list = Article.objects.published()
-#     paginator = Paginator(articles_list, 5)
-#     articles = paginator.get_page(page)
-#     context = {
-#         "articles": articles,
-#     }
-#     return render(request, 'blog/home.html', context)
 
 class ArticleList(ListView):
-    # model = Article
-    # template_name = "blog/home.html"
-    # context_object_name = "articles"
     queryset = Article.objects.published()
     paginate_by = 4
 
 
-def detail(request, slug):
-    context = {
-        "article": get_object_or_404(Article.objects.published(), slug=slug)
-    }
-    return render(request, 'blog/detail.html', context)
+class ArticleDetailView(DetailView):
+    def get_object(self, queryset=None):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Article.objects.published(), slug=slug)
 
 
 def category(request, slug, page=1):
@@ -39,3 +28,19 @@ def category(request, slug, page=1):
         "articles": articles
     }
     return render(request, 'blog/category.html', context)
+
+# def home(request, page=1):
+#     articles_list = Article.objects.published()
+#     paginator = Paginator(articles_list, 5)
+#     articles = paginator.get_page(page)
+#     context = {
+#         "articles": articles,
+#     }
+#     return render(request, 'blog/home.html', context)
+
+
+# def detail(request, slug):
+#     context = {
+#         "article": get_object_or_404(Article.objects.published(), slug=slug)
+#     }
+#     return render(request, 'blog/detail.html', context)
