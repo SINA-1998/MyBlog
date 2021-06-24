44c7f6118511156a20dc182f7ffed62bc4fcd19f
SINA-1998 <sinafallahnezhad76@gmail.com>
2021-06-24 10:49:37 +0430

    Add publish and draft action to article panel admin.

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,32 @@ from .models import Article, Category
 
 
 # Register your models here.
+
+# For disable action use this statement 👇
+# admin.site.disable_action("action name")
+
+def make_published(modeladmin, request, queryset):
+    rows_updated = queryset.update(status="p")
+    if rows_updated == 1:
+        message_bit = "شد."
+    else:
+        message_bit = "شدند."
+    modeladmin.message_user(request, f"{rows_updated} مقاله منتشر {message_bit}")
+
+
+def make_draft(modeladmin, request, queryset):
+    rows_updated = queryset.update(status="d")
+    if rows_updated == 1:
+        message_bit = "شد."
+    else:
+        message_bit = "شدند."
+    modeladmin.message_user(request, f"{rows_updated} مقاله پیش‌نویس {message_bit}")
+
+
+make_published.short_description = "انتشار مقالات انتخاب شده"
+make_draft.short_description = "پیش‌نویس شدن مقالات انتخاب شده"
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("position", "title", "slug", "parent", "status")
     list_filter = (["status"])
@@ -19,6 +45,7 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ("title", "description")
     prepopulated_fields = {"slug": ("title",)}
     ordering = ["status", "publish"]
+    actions = [make_published, make_draft]
 
     def category_to_str(self, obj):
         return "، ".join([category.title for category in obj.category_published()])
