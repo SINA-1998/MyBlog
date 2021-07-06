672122f653e9403fe7044b8928eb593424ccfecd
SINA-1998 <sinafallahnezhad76@gmail.com>
2021-07-06 18:51:32 +0430

    Create custom template tag for active windows in sidebar and Add delete button in update article page for super user.

--- account/urls.py
+++ account/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
 
 urlpatterns += [
     path('', ArticleList.as_view(), name="home"),
-    path('article/create', ArticleCreate.as_view(), name="article_crate"),
+    path('article/create', ArticleCreate.as_view(), name="article_create"),
     path('article/update/<int:pk>', ArticleUpdate.as_view(), name="article_update"),
     path('article/delete/<int:pk>', ArticleDelete.as_view(), name="article_delete")
 ]
--- blog/templatetags/base_tags.py
+++ blog/templatetags/base_tags.py
@@ -19,3 +19,13 @@ def category_navbar():
     return {
         "category": Category.objects.filter(status=True)
     }
+
+
+@register.inclusion_tag("../templates/registration/partials/link.html")
+def link(request, link_name, content):
+    return {
+        "request": request,
+        "link_name": link_name,
+        "content": content,
+        "link": "account:{}".format(link_name),
+    }
