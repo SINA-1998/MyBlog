433eec8f75fb775a5e24d98e379f1ff6af990b12
SINA-1998 <sinafallahnezhad76@gmail.com>
2021-07-07 14:27:00 +0430

    Add profile view.

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import views
 from django.urls import path
-from .views import ArticleList, ArticleCreate, ArticleUpdate, ArticleDelete
+from .views import ArticleList, ArticleCreate, ArticleUpdate, ArticleDelete, Profile, Login
 
 app_name = 'account'
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', Login.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     #
     # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
@@ -20,5 +20,6 @@ urlpatterns += [
     path('', ArticleList.as_view(), name="home"),
     path('article/create', ArticleCreate.as_view(), name="article_create"),
     path('article/update/<int:pk>', ArticleUpdate.as_view(), name="article_update"),
-    path('article/delete/<int:pk>', ArticleDelete.as_view(), name="article_delete")
+    path('article/delete/<int:pk>', ArticleDelete.as_view(), name="article_delete"),
+    path('profile/', Profile.as_view(), name="profile")
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Article
+from .models import User
+from django.contrib.auth.views import LoginView
 from .mixins import FieldsMixin, FormValidMixin, AuthorAccessMixin, SuperuserAccessMixin
+from .forms import ProfileForm
 
 
 # Create your views here.
@@ -33,3 +34,30 @@ class ArticleDelete(SuperuserAccessMixin, DeleteView):
     model = Article
     success_url = reverse_lazy("account:home")
     template_name = "registration/article_confirm_delete.html"
+
+
+class Profile(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = "registration/profile.html"
+    form_class = ProfileForm
+    success_url = reverse_lazy('account:profile')
+
+    def get_object(self, queryset=None):
+        return User.objects.get(pk=self.request.user.pk)
+
+    def get_form_kwargs(self):
+        kwargs = super(Profile, self).get_form_kwargs()
+        kwargs.update({
+            'user': self.request.user
+        })
+        return kwargs
+
+
+class Login(LoginView):
+    def get_success_url(self):
+        user = self.request.user
+
+        if user.is_superuser or user.is_author:
+            return reverse_lazy('account:home')
+        else:
+            return reverse_lazy('account:profile')
